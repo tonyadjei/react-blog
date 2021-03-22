3af143145671a6ef67c86934a2aa6a211fa03bd4
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2021-03-22 04:25:53 +0100

    added 'loading... .' to simulate API call delay

--- src/Home.js
+++ src/Home.js
@@ -2,30 +2,33 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'How to learn React', body: 'lorem....ipsum', author: 'James', id: 1},
-        {title: 'God loves you', body: 'lorem....ipsum', author: 'Terrence', id: 2},
-        {title: 'How to cook pancakes', body: 'lorem....ipsum', author: 'James', id: 3}
-    ]);
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
-    const [name, setName] = useState("tony");
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id !== id);
-        setBlogs(newBlogs);
-    }
     useEffect(() => {
-        console.log('Use effect run');
-        console.log(name);
-    }, [name]); //dependency arrays allow us to run useEffect for certain kinds on renders only.
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    // if(res.ok){}
+                    console.log(res);
+                    return res.json();
+                })
+                .then((data) => {
+                    setBlogs(data);
+                    setIsPending(false);
+                })
+                .catch(err => {
+                    console.log(err.message);
+                })
+        }, 1000);
+    }, []); 
+    //dependency arrays allow us to run useEffect for certain kinds of renders only.
     //an empty dependency array will cause useEffect to run only once(i.e. the component's initial render only)
 
     return ( 
         <div className="Home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-            <button onClick={() => setName("Elon Musk")}>change name</button>
-            <p>{name}</p>
-
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
      );
 }
