e601fc00962dbb6cafd06d06bdc771340d042a85
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2021-03-22 03:39:37 +0100

    added JSON server to practice API calls and tested useEffect

--- src/Home.js
+++ src/Home.js
@@ -7,18 +7,24 @@ const Home = () => {
         {title: 'God loves you', body: 'lorem....ipsum', author: 'Terrence', id: 2},
         {title: 'How to cook pancakes', body: 'lorem....ipsum', author: 'James', id: 3}
     ]);
+
+    const [name, setName] = useState("tony");
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id);
         setBlogs(newBlogs);
     }
     useEffect(() => {
         console.log('Use effect run');
-        console.log(blogs);
-    }, []); //dependency arrays to run useEffect for certain kinds on renders only.
+        console.log(name);
+    }, [name]); //dependency arrays allow us to run useEffect for certain kinds on renders only.
+    //an empty dependency array will cause useEffect to run only once(i.e. the component's initial render only)
 
     return ( 
         <div className="Home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+            <button onClick={() => setName("Elon Musk")}>change name</button>
+            <p>{name}</p>
 
         </div>
      );
