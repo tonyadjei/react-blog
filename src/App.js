810192a486e1d847153920a8f811b13777e1f1b7
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2021-03-23 16:14:44 +0100

    created blog details

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import Navbar from './Navbar';
 import Home from './Home';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+          </Switch>
+        </div>
     </div>
-  );
+    </Router>
+    
+  ); 
 }
 
 export default App;
--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,9 @@ const useFetch = (url) => { //custom hooks must begin with 'use'
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        const abortController = new AbortController();
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortController.signal })
                 .then(res => {
                     if(!res.ok){
                         throw Error('could not fetch the data for that resource.');
@@ -20,10 +21,16 @@ const useFetch = (url) => { //custom hooks must begin with 'use'
                     setIsPending(false);
                 })
                 .catch(err => {
-                    setIsPending(false);
-                    setError(err.message);
+                    if(err.name === 'AbortError'){
+                        console.log('fetch() aborted')
+                    }else {
+                        setIsPending(false);
+                        setError(err.message);
+                    } 
                 })
         }, 1000);
+        return () => abortController.abort();
+
     }, [url]);
     //dependency arrays allow us to run useEffect for certain kinds of renders only.
     //an empty dependency array will cause useEffect to run only once(i.e. the component's initial render only)
